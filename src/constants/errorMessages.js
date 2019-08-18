c350facd662eba29082ec5713ba973666ac1a3a3
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2019-08-18 12:28:15 +0200

    feat: add constants for error messages

---
+++ src/constants/errorMessages.js
@@ -0,0 +1,3 @@
+export const ERROR_BEER_NAME = 'This must only contain letters, numbers, hyphens and spaces (example: Punk IPA 2007 - 2010)'
+export const ERROR_BEER_BEFORE_BEWED = 'This must only contain numbers and hyphens on date format MM-YYYY (example: 04-2007)'
+export const ERROR_BEER_LIST_EMPTY = 'We cannot find your beers. Please try again'
