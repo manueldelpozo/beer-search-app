ad0b30caf4a3b0bb24829b0335b886c2b0ae804b
Manuel del Pozo Gil de Pareja <mdp.webdeveloper@gmail.com>
2019-08-19 02:59:06 +0200

    feat: scroll to new list after search

--- src/components/BeerList.js
+++ src/components/BeerList.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useState, useEffect, useRef } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
 import List from '@material-ui/core/List'
@@ -23,16 +23,25 @@ const useStyles = makeStyles(theme => ({
     },
 }))
 
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)
+
 const BeerList = ({beers, isLoading}) => {
     const classes = useStyles()
     const [selectedBeerId, setSelectedBeerId] = useState(0)
+    const myRef = useRef(null)
+    
+    useEffect(() => {
+        if (beers.length > 0) {
+            scrollToRef(myRef)
+        }
+    }, [beers])
 
     const onSelectBeer = (event, id) => {
         setSelectedBeerId(id)
     }
 
     return (
-        <List className={classes.root} data-testid="beer-list">
+        <List className={classes.root} data-testid="beer-list" ref={myRef}>
             {isLoading ?
             <CircularProgress className={classes.progress} /> :
             beers.map(beer =>
